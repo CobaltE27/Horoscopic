de575c29bf4e700a39c4edc3202c8a9c69e892d4
CobaltE27 <potassiume19@gmail.com>
2024-10-07 21:17:37 -0600

    Added some life table operations and improved table bound detection

--- horoscope.py
+++ horoscope.py
@@ -1,28 +1,71 @@
+import re
 from pymort import MortXML
 
 def getYearMortality(age: int, tableXml: MortXML):
-    startingAge = tableXml.Tables[0].MetaData.AxisDefs[0].MinScaleValue
-    ageIndex = age - startingAge
+    startingAge = getMinimumAge(tableXml)
+    ageIndex = age - startingAge + 1 #data indecies start at 1
     return tableXml.Tables[0].Values["vals"][ageIndex]
 
 def getDayMortality(age: int, tableXml: MortXML):
     return getYearMortality(age, tableXml) / 365.0
 
 def getRangeMortality(age: int, tableXml: MortXML, years: int):
-    maxTableAge = tableXml.Tables[0].MetaData.AxisDefs[0].MaxScaleValue
-    if (age + years - 1 > maxTableAge):
+    maxTableAge = getMaximumAge(tableXml)
+    if (age + years - 1 > maxTableAge): #age range exceeds table size, guaranteed death for now
         return 1.0
+    
     survivalChance = 1.0 - getYearMortality(age, tableXml)
     for futureAge in range (age + 1, age + years - 1):
         survivalChance *= 1 - getYearMortality(futureAge, tableXml)
+    
     return 1.0 - survivalChance
+
+def getYearOutlived(age: int, lifeTable: MortXML):
+    maxTableAge = getMaximumAge(lifeTable)
+    startingAge = getMinimumAge(lifeTable)
+    poolSize = lifeTable.Tables[0].Values["vals"][1]
+
+    numAlive = 0
+    if (age > maxTableAge):
+        numAlive = lifeTable.Tables[0].Values["vals"][maxTableAge - startingAge + 1] #data indecies start at 1
+    elif (age < startingAge):
+        return 0.0
+    else:
+        ageIndex = (age - startingAge) + 1 #data indecies start at 1
+        numAlive = lifeTable.Tables[0].Values["vals"][ageIndex]
+
+    return (poolSize - numAlive) / poolSize #fraction of pool you've outlived
+
+def getMinimumAge(tableXml: MortXML):
+    minAgeRE = r"[Mm]in(?:imum)? [Aa]ge\D*(\d\d\d?)" #standard way of listing in description
+    description = str(tableXml.ContentClassification.TableDescription)
+    match = re.search(minAgeRE, description)
+    if match:
+        return int(match.group(1))
+    else:
+        return tableXml.Tables[0].MetaData.AxisDefs[0].MinScaleValue #less reliable, so only use if description doesn't list it
+
+def getMaximumAge(tableXml: MortXML):
+    maxAgeRE = r"[Mm]ax(?:imum)? [Aa]ge\D*(\d\d\d?)"
+    description = str(tableXml.ContentClassification.TableDescription)
+    match = re.search(maxAgeRE, description)
+    if match:
+        return int(match.group(1))
+    else:
+        return tableXml.Tables[0].MetaData.AxisDefs[0].MaxScaleValue #less reliable, so only use if description doesn't list it
+
 
 print("getting")
 exampleXml = MortXML.from_id(3153)
+exampleLifeTable = MortXML.from_id(2829)
 print('keywords: ' + str(exampleXml.ContentClassification.KeyWords))
-print('metadata: ' + str(exampleXml.Tables[0].MetaData))
+print('mort metadata: ' + str(exampleXml.Tables[0].MetaData))
+print('life metadata: ' + str(exampleLifeTable.Tables[0].MetaData))
+print('life description: ' + str(exampleLifeTable.ContentClassification.TableDescription))
 print('content type: ' + exampleXml.ContentClassification.ContentType)
 print('data for given age: ' + str(getYearMortality(20, exampleXml)))
-print('chance to die in 20 years at 20: ' + str(getRangeMortality(20, exampleXml, 20)))
-print('chance to die in 60 years at 20: ' + str(getRangeMortality(20, exampleXml, 60)))
-print('chance to die in 80 years at 20: ' + str(getRangeMortality(20, exampleXml, 80)))
+print('chance to die in 80 years at 20: ' + str(getRangeMortality(1, exampleXml, 80)))
+print('chance to die in 110 years at 20: ' + str(getRangeMortality(20, exampleXml, 100)))
+print("fraction outlived by 10 year old: " + str(getYearOutlived(10, exampleLifeTable)))
+print("fraction outlived by 15 year old: " + str(getYearOutlived(15, exampleLifeTable)))
+print("fraction outlived by 89 year old: " + str(getYearOutlived(98, exampleLifeTable)))
