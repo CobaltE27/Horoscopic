6aeda3dc5857a978fcf43f88d1bdb932abc94718
CobaltE27 <potassiume19@gmail.com>
2024-10-10 23:43:38 -0600

    began user interface inplementation

--- horoscope.py
+++ horoscope.py
@@ -3,6 +3,7 @@ import json
 from enum import Enum
 import datetime
 from pymort import MortXML
+import decimal
 
 Sex = Enum("Sex", ["ALL", "MALE", "FEMALE"])
 thisYear = datetime.datetime.now().year
@@ -26,6 +27,21 @@ def as_enum(d):
         return d
 #enum encoding/decoding from https://stackoverflow.com/questions/24481852/serialising-an-enum-member-to-json/24482806#24482806
 
+def toNonsciNotationString(input: float):
+    dec = decimal.Context().create_decimal(input)
+    return format(dec, 'f')
+
+def convertToPercentString(probability: float):
+    preliminary = toNonsciNotationString(probability * 100)
+    counter = 0
+    for index in range(preliminary.find(".") + 1, len(preliminary) - 1):
+        if counter > 0 or preliminary[index] != '0':
+            counter += 1
+        if counter > 2:
+            preliminary = preliminary[:index]
+            break
+    return preliminary + "%"
+
 def findBestTableIn(filename: str, sex: Sex):
     with open(filename, "r") as tableJson:
         mortTables = dict(json.loads(tableJson.read(), object_hook=as_enum))
@@ -39,7 +55,7 @@ def findBestTableIn(filename: str, sex: Sex):
                 bestDistance = distance
     return bestId
 
-def GatherTablesOfType(contentType: str):
+def gatherTablesOfType(contentType: str):
     contentType = contentType.casefold()
     misses = 0
     tables = dict()
@@ -55,35 +71,35 @@ def GatherTablesOfType(contentType: str):
             table = MortXML.from_id(id) #should throw exception if it doesn't exist
             misses = 0
             if table.ContentClassification.ContentType.casefold() == contentType:
-                tables[id] = {"year": GetTableYear(table), "sex": GetTableSex(table)}
+                tables[id] = {"year": getTableYear(table), "sex": getTableSex(table)}
         except FileNotFoundError:
             misses += 1
         id += 1
     
     return tables
 
-def GetYearMortality(age: int, tableXml: MortXML):
-    startingAge = GetMinimumAge(tableXml)
+def getYearMortality(age: int, tableXml: MortXML):
+    startingAge = getMinimumAge(tableXml)
     ageIndex = age - startingAge + 1 #data indecies start at 1
     return tableXml.Tables[0].Values["vals"][ageIndex]
 
-def GetDayMortality(age: int, tableXml: MortXML):
-    return GetYearMortality(age, tableXml) / 365.0
+def getDayMortality(age: int, tableXml: MortXML):
+    return getYearMortality(age, tableXml) / 365.0
 
-def GetRangeMortality(age: int, tableXml: MortXML, years: int):
-    maxTableAge = GetMaximumAge(tableXml)
+def getRangeMortality(age: int, tableXml: MortXML, years: int):
+    maxTableAge = getMaximumAge(tableXml)
     if (age + years - 1 > maxTableAge): #age range exceeds table size, guaranteed death for now
         return 1.0
     
-    survivalChance = 1.0 - GetYearMortality(age, tableXml)
+    survivalChance = 1.0 - getYearMortality(age, tableXml)
     for futureAge in range (age + 1, age + years - 1):
-        survivalChance *= 1 - GetYearMortality(futureAge, tableXml)
+        survivalChance *= 1 - getYearMortality(futureAge, tableXml)
     
     return 1.0 - survivalChance
 
-def GetYearOutlived(age: int, lifeTable: MortXML):
-    maxTableAge = GetMaximumAge(lifeTable)
-    startingAge = GetMinimumAge(lifeTable)
+def getYearOutlived(age: int, lifeTable: MortXML):
+    maxTableAge = getMaximumAge(lifeTable)
+    startingAge = getMinimumAge(lifeTable)
     poolSize = lifeTable.Tables[0].Values["vals"][1]
 
     numAlive = 0
@@ -104,7 +120,7 @@ def GetYearOutlived(age: int, lifeTable: MortXML):
 
     return (poolSize - numAlive) / poolSize #fraction of pool you've outlived
 
-def GetMinimumAge(tableXml: MortXML):
+def getMinimumAge(tableXml: MortXML):
     minAgeRE = r"[Mm]in(?:imum)? [Aa]ge\D*(\d\d\d?)" #standard way of listing in description
     description = str(tableXml.ContentClassification.TableDescription)
     match = re.search(minAgeRE, description)
@@ -113,7 +129,7 @@ def GetMinimumAge(tableXml: MortXML):
     else:
         return tableXml.Tables[0].MetaData.AxisDefs[0].MinScaleValue #less reliable, so only use if description doesn't list it
 
-def GetMaximumAge(tableXml: MortXML):
+def getMaximumAge(tableXml: MortXML):
     maxAgeRE = r"[Mm]ax(?:imum)? [Aa]ge\D*(\d\d\d?)"
     description = str(tableXml.ContentClassification.TableDescription)
     match = re.search(maxAgeRE, description)
@@ -122,7 +138,7 @@ def GetMaximumAge(tableXml: MortXML):
     else:
         return tableXml.Tables[0].MetaData.AxisDefs[0].MaxScaleValue #less reliable, so only use if description doesn't list it
 
-def GetTableYear(table: MortXML):
+def getTableYear(table: MortXML):
     yearRE = re.compile(r"(?:20|19|18)\d\d")
     matches = dict() #matches ordered by priority
 
@@ -153,7 +169,7 @@ def GetTableYear(table: MortXML):
     
     return int(consensus)
 
-def GetTableSex(table: MortXML):
+def getTableSex(table: MortXML):
     maleRE = re.compile(r"[^e]male", re.I)
     femaleRE = re.compile(r"female", re.I)
     description = str(table.ContentClassification.TableDescription)
@@ -166,28 +182,57 @@ def GetTableSex(table: MortXML):
     return Sex.ALL
 
 
-print("getting")
-exampleXml = MortXML.from_id(3153)
-exampleLifeTable = MortXML.from_id(2829)
 # with open("mortalities", 'w') as mortalityTables:
 #     mortalityTables.write(json.dumps(GatherTablesOfType("healthy lives mortality"), cls=EnumEncoder, indent=2))
 # with open("lifeTables", 'w') as lifeTables:
 #     lifeTables.write(json.dumps(GatherTablesOfType("life table"), cls=EnumEncoder, indent=2))
 
-#print(json.dumps(GatherTablesOfType("healthy lives mortality"), cls=EnumEncoder))
-print(MortXML.from_id(2921).Tables[0].Values.axes[0][0])
-print("best now: " + str(findBestTableIn("mortalities", Sex.FEMALE)))
-print("best life now: " + str(findBestTableIn("lifeTables", Sex.FEMALE)))
-print("mort type: " + str(exampleXml.ContentClassification.ContentType))
-print('mort metadata: ' + str(exampleXml.Tables[0].MetaData))
-print('life metadata: ' + str(exampleLifeTable.Tables[0].MetaData))
-print('data for given age: ' + str(GetYearMortality(20, exampleXml)))
-print('chance to die in 80 years at 20: ' + str(GetRangeMortality(1, exampleXml, 80)))
-print('chance to die in 110 years at 20: ' + str(GetRangeMortality(20, exampleXml, 100)))
-print("fraction outlived by 10 year old: " + str(GetYearOutlived(10, exampleLifeTable)))
-print("fraction outlived by 15 year old: " + str(GetYearOutlived(15, exampleLifeTable)))
-print("fraction outlived by 89 year old: " + str(GetYearOutlived(98, exampleLifeTable)))
-print("lifetable year: " + str(GetTableYear(exampleLifeTable)))
-print("mort year: " + str(GetTableYear(exampleXml)))
-print("mort sex: " + str(GetTableSex(exampleXml)))
-print("lifetable sex: " + str(GetTableSex(exampleLifeTable)))
+exampleXml = MortXML.from_id(3153)
+exampleLifeTable = MortXML.from_id(2829)
+# print(json.dumps(GatherTablesOfType("healthy lives mortality"), cls=EnumEncoder))
+# print(MortXML.from_id(2921).Tables[0].Values.axes[0][0])
+# print("best now: " + str(findBestTableIn("mortalities", Sex.FEMALE)))
+# print("best life now: " + str(findBestTableIn("lifeTables", Sex.FEMALE)))
+# print("mort type: " + str(exampleXml.ContentClassification.ContentType))
+# print('mort metadata: ' + str(exampleXml.Tables[0].MetaData))
+# print('life metadata: ' + str(exampleLifeTable.Tables[0].MetaData))
+# print('data for given age: ' + str(getYearMortality(20, exampleXml)))
+# print('chance to die in 80 years at 20: ' + str(getRangeMortality(1, exampleXml, 80)))
+# print('chance to die in 110 years at 20: ' + str(getRangeMortality(20, exampleXml, 100)))
+# print("fraction outlived by 10 year old: " + str(getYearOutlived(10, exampleLifeTable)))
+# print("fraction outlived by 15 year old: " + str(getYearOutlived(15, exampleLifeTable)))
+# print("fraction outlived by 89 year old: " + str(getYearOutlived(98, exampleLifeTable)))
+# print("lifetable year: " + str(getTableYear(exampleLifeTable)))
+# print("mort year: " + str(getTableYear(exampleXml)))
+# print("mort sex: " + str(getTableSex(exampleXml)))
+# print("lifetable sex: " + str(getTableSex(exampleLifeTable)))
+
+userAge = 0
+while(1):
+    try:
+        userAge = int(input("What is your age? "))
+        break
+    except ValueError:
+        print("Please enter a valid integer!")
+
+userSex = Sex.ALL
+while(1):
+    userSex = input("What were the circumstances of your birth? (m/f) ")
+    if userSex == "m":
+        userSex = Sex.MALE
+        break
+    elif userSex == "f":
+        userSex = Sex.FEMALE
+        break
+    else:
+        print("Please enter \"m\" or \"f\"")
+
+mort = MortXML.from_id(findBestTableIn("mortalities", userSex))
+life = MortXML.from_id(findBestTableIn("lifeTables", userSex))
+print("Your chance to die today: " + convertToPercentString(getDayMortality(userAge, mort)))
+print("Your chance to die this year: " + convertToPercentString(getYearMortality(userAge, mort)))
+print("Your chance to die within the decade: " + convertToPercentString(getRangeMortality(userAge, mort, 10)))
+print("Your chance to die within the next 3 decades: " + convertToPercentString(getRangeMortality(userAge, mort, 30)))
+print("Your chance to die within the next 6 decades: " + convertToPercentString(getRangeMortality(userAge, mort, 60)))
+print("Your chance to die within the next century: " + convertToPercentString(getRangeMortality(userAge, mort, 100)))
+print("You have outlived approximately " + convertToPercentString(getYearOutlived(userAge, life)) + " of people born under the same circumstances.")
